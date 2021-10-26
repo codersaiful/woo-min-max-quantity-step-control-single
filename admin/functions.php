dbd152f8c61636ea101d986e3eee0980e68fb4b7
Saiful Islam <codersaiful@gmail.com>
2021-10-26 12:37:36 +0600

    prefix suffix image in free version

--- admin/functions.php
+++ admin/functions.php
@@ -31,3 +31,20 @@ $image_link = WC_MMQ_BASE_URL . 'assets/images/features/setting-bottom.jpg'
 
 }
 add_action( 'wcmmq_setting_bottom_row','wcmmq_category_choose_image' );
+
+function wcmmq_quantity_prefix_sufix_feautre($saved_data){
+    if( defined( 'WC_MMQ_PRO_VERSION' ) ) return;
+$image_link = WC_MMQ_BASE_URL . 'assets/images/features/quantity-prefix-sufix.jpg'
+?>
+<div class="wcmmq-premium">
+    <div>
+        <img src="<?php echo esc_url($image_link); ?>">
+    </div>
+</div>
+
+<?php    
+
+}
+add_action( 'wcmmq_form_panel_before_message','wcmmq_quantity_prefix_sufix_feautre' );
+
+
