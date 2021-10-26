fa7db0ed7b502cae017a36dfe0180465816c7325
Saiful Islam <codersaiful@gmail.com>
2021-10-26 11:42:04 +0600

    category choose image in free version using action hook

--- admin/functions.php
+++ admin/functions.php
@@ -1,28 +1,17 @@
 <?php
 /**
- * Use in your function.php file.
- * To show all Terms to set condition
- * Rememeber: if match mutliple terms for a product, 
- * Only one will work
- * @param bool true|false True and False only
- * @param Array $saved_data Get Array of Saved Data of Forms
+ * Obviously need tr and td here
  */
-//add_filter( 'wcmmq_all_terms', '__return_true' );
+function wcmmq_category_choose_image($saved_data){
+    if( defined( 'WC_MMQ_PRO_VERSION' ) ) return;
+$image_link = WC_MMQ_BASE_URL . 'assets/images/features/setting-bottom.jpg'
+?>
+<tr class="wcmmq-premium">
+    <td colspan="2">
+        <img src="<?php echo esc_url($image_link); ?>">
+    </td>
+</tr>
+<?php    
 
-//if( !function_exists( 'wcmmp_term_list' ) ){
-    /**
-     * To add new Terms in Term Condition for Min max
-     * User able to add any type of Taxomony, 
-     * Such: product_cat, product_tag, pa_color, pa_size etc
-     * 
-     * @param array $ourTermList
-     * @param Array $saved_data Get Array of Saved Data of Forms
-     * @return Array
-     */
-//    function wcmmp_term_list( $ourTermList ){
-//        $ourTermList['product_tag'] = 'Tag';
-//        return $ourTermList;
-//    }
-//    add_filter( 'wcmmq_terms_list', 'wcmmp_term_list' );
-   
-// }
+}
+add_action( 'wcmmq_setting_bottom_row','wcmmq_category_choose_image' );
