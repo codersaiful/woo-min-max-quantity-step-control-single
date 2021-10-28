aae8a5b5efe854642835554ab78e185cb9c2c17a
Saiful Islam <codersaiful@gmail.com>
2021-10-28 10:18:14 +0600

    menu update

--- admin/functions.php
+++ admin/functions.php
@@ -10,11 +10,12 @@ function wcmmq_before_form_free_content(){
 <div class="wcmmq-nav">
     <ul>
         <li><a href="https://min-max-quantity.codeastrology.com/docs/" target="_blank">Documentation</a></li>
-        <li><a href="https://min-max-quantity.codeastrology.com/" target="_blank">Checkout Pro Features</a></li>
-        <li><a href="https://wordpress.org/support/plugin/woo-min-max-quantity-step-control-single/reviews/#new-post" target="_blank">Rate Our Plugin on wordpress.org</a></li>
+        <li><a href="https://wordpress.org/support/plugin/woo-min-max-quantity-step-control-single/reviews/#new-post" target="_blank">Rate on wordpress.org</a></li>
         <li><a href="https://wordpress.org/support/plugin/woo-min-max-quantity-step-control-single/" target="_blank">WordPress Forum</a></li>
-        <li><a href="https://codeastrology.com/support/" target="_blank">Need Help? Contact with us</a></li>
-        <li><a href="https://github.com/codersaiful/woo-min-max-quantity-step-control/issues/new" target="_blank">Request New Features</a></li>
+        <li><a href="https://codeastrology.com/support/" target="_blank">Need Help?</a></li>
+        <li><a href="https://github.com/codersaiful/woo-min-max-quantity-step-control/issues/new" target="_blank">Request Features</a></li>
+        <li class="wcmmq-checkout-pro-features"><a href="https://min-max-quantity.codeastrology.com/" target="_blank">Pro Features</a></li>
+        <li class="wcmmq-pro-buy-now"><a href="https://codecanyon.net/item/woocommerce-min-max-quantity-step-control/22962198" target="_blank">Buy Now</a></li>
     </ul>
 </div>    
 <?php    
