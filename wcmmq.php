966296e44abcc9a00f9fa54fa086d9bf73c8d661
Saiful Islam <codersaiful@gmail.com>
2022-05-17 18:16:12 +0600

    6.4.1 wc tested updated

--- wcmmq.php
+++ wcmmq.php
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         5.9.1
  * WC requires at least: 3.0.0
- * WC tested up to: 	 6.2.1
+ * WC tested up to: 	 6.4.1
  * 
  * Text Domain: wcmmq
  * Domain Path: /languages/
