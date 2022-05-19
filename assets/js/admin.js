2d87477e4a00068a439f288937cfb800d2371434
rafiul <bmrafiul.alam@gamail.com>
2022-05-19 17:16:59 +0600

    Min Max value contions

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,19 +8,22 @@
          * Support terms -> on after change,
          * form will be submit
          */
-        $(document.body).on('change','#wcmmq_supported_terms',function(){
+         $(document.body).on('change','#wcmmq_supported_terms',function(){
             $('#wcmmq_form_submit_button').trigger('click');
-
         });
+        
         $(document.body).on('submit', 'form#wcmmq-main-configuration-form', function (){
             var min_val = $(this).find('.config_min_qty').val();
             var max_val = $(this).find('.config_max_qty').val();
-            if (max_val != '') {
-                if (min_val > max_val) {
+            if(min_val!=''){
+                //Just added parseInt() beacuse of data type was string.
+                if (parseInt(min_val) > parseInt(max_val)) {
                     alert("Please make sure that your minimum quantity is smaller than maximum quantity!");
                     return false;
                 }
             }
         });
+
+       
     });
 })(jQuery);
