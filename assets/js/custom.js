5f3a124789b29523288cf4a1e822c92c1d744c52
Saiful Islam <codersaiful@gmail.com>
2023-01-08 13:24:43 +0600

    decimal issue solved when our plus minus not enabled

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,12 +22,99 @@
                 parentQuantity.append('<input type="text" value="' + val_with_coma + '" class="wcmmq-second-input-box" id="wcmmq-second-input-id">');
             });
 
-            $(document.body).on('keyup','.wcmmq-second-input-box',function(){
-                var parentQuantity = $(this).parents('.quantity');
-                var secondInputVal = $(this).val();
-                var secondValWithDot = secondInputVal.replace(/,/g, '.');
-                parentQuantity.find('.wcmmq-main-input-box').val(secondValWithDot);
+            /**
+             * It's our custom input box with text type
+             * and we will transfer this text to main input(number) with convert comma to dot
+             * 
+             * @since 3.5.2
+             */
+            $(document.body).on('keyup','.wcmmq-second-input-box',function(Event){
+                
+                /**
+                 * First, I will findout, If any user click on
+                 * up or down arrow.
+                 * So that, we can set behavier like number input box.
+                 */
+                var arrowPress = false;
+                if(typeof Event === 'object' && typeof Event.originalEvent === 'object'){
+                    var originalEvent = Event.originalEvent;
+                    if(originalEvent.keyCode === 38 || originalEvent.code === 'ArrowUp'){
+                          arrowPress = 'ArrowUp';
+                    }else if(originalEvent.keyCode === 40 || originalEvent.code === 'ArrowDown'){
+                        arrowPress = 'ArrowDown';
+                    }
+
+                }
+                
+                /**
+                 * Checking Down/Up arrow button
+                 * If not click on up or down arrow button
+                 * and if click on any number, then this bellow code will write in our 
+                 * main input(number) box with . or , 
+                 * 
+                 * @since3.5.2
+                 */
+                if( !arrowPress ){
+                    var parentQuantity = $(this).parents('.quantity');
+                    var secondInputVal = $(this).val();
+                    var secondValWithDot = secondInputVal.replace(/,/g, '.');
+                    parentQuantity.find('.wcmmq-main-input-box').val(secondValWithDot);
+                }else{
+                    var secondInboxObject = $(this);
+                    Event.preventDefault();
+                    plusMinusOnArrowCalculate(arrowPress,secondInboxObject);
+                }
             });
+        }
+
+        /**
+         * ONLY for coman decimal separator, Not for else
+         * 
+         * First requirement:
+         * It will work ONLY a User enable comma as Decimal
+         * Otherwise, this is not will impact any more.
+         * 
+         * @since 3.5.2
+         * 
+         */
+        function plusMinusOnArrowCalculate(type,secondInboxObject){
+
+            var qty = secondInboxObject.closest('.wcmmq-coma-separator-activated').find('input.input-text.qty.text.wcmmq-main-input-box');
+            // Read value and attributes min, max, step.
+            var val = parseFloat(qty.val());
+            var max = parseFloat(qty.attr("max"));
+            var min = parseFloat(qty.attr("min"));
+            var step = parseFloat(qty.attr("step"));
+            console.log(min,val,max,step);
+
+            if( type === 'ArrowUp'){
+                if (val === max){
+                    return false;
+                }
+                    
+                if (isNaN(val)) {
+                    qty.val(step).trgger('change');
+                    return false;
+                }
+
+                qty.val(val + step);
+            }else if( type === 'ArrowDown'){
+                if (val === min){
+                    return false;
+                }
+                if (isNaN(val)) {
+                    qty.val(min).trgger('change');
+                    return false;
+                }
+                if (val - step < min) {
+                    qty.val(min);
+                } else {
+                    qty.val(val - step);
+                }
+            }
+
+            qty.val(Math.round(qty.val() * 100000) / 100000);
+            qty.trigger("change");
         }
 
         /**
