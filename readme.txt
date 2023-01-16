cbe5c2c230fd141d05fac865b8f933fb897383e4
Saiful Islam <codersaiful@gmail.com>
2023-01-16 15:22:42 +0600

    Version: 3.6

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: WooCommerce, minimum quantity, maximum quantity, woocommrce quantity, cust
 Requires at least: 4.0.0
 Tested up to: 6.1
 Requires PHP: 7.0
-Stable tag: 3.5
+Stable tag: 3.6
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- wcmmq.php
+++ wcmmq.php
@@ -7,7 +7,7 @@
  * Author URI: https://codeastrology.com
  * Tags: WooCommerce, minimum quantity, maximum quantity, woocommrce quantity, input step control for WC, customize wc quantity, wc qt, max qt, min qt, maximum qt, minimum qt
  * 
- * Version: 3.5
+ * Version: 3.6
  * Requires at least:    4.0.0
  * Tested up to:         6.1.1
  * WC requires at least: 3.0.0
