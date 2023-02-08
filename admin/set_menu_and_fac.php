eca01c93b1c6527929fe95852cb5fe683626e9a2
Fazle Bari <fazlebarisn@gmail.com>
2023-02-08 15:42:21 +0600

    doc link added

--- admin/set_menu_and_fac.php
+++ admin/set_menu_and_fac.php
@@ -175,9 +175,9 @@ function wcmmq_faq_page_details(){
                                     <div class="slider round"><!--ADDED HTML -->
                                         <span class="on"><?php echo esc_html__('ON','wcmmq');?></span><span class="off"> <?php echo esc_html__('OFF','wcmmq');?></span><!--END-->
                                     </div>
-                                </label>
+                                </label><?php wpt_doc_link('https://codeastrology.com/min-max-quantity/add-quantity-box-on-shop-page/'); ?>
                                 <p style="color: #228b22;">
-                                For ajax add to cart, Enable from <strong>WooCommerce->Settings->Products->General->Add to cart behaviour</strong>.<br>
+                                For ajax add to cart, Enable from <strong>WooCommerce->Settings->Products->Add to cart behaviour</strong>.<br>
                                 For Variable product Quantity Box with Variation change box. Need premium version.<br>
                                 If you need Plus Minus Button for your Quantity Box install <a href="https://wordpress.org/plugins/wc-quantity-plus-minus-button/" target="_blank">Quantity Plus Minus Button for WooCommerce by CodeAstrology</a>
                                 </p>
