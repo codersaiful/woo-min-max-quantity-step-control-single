5cae4ace85b16308d82cf385e640fc42164660d4
Saiful Islam <codersaiful@gmail.com>
2023-08-01 08:04:24 +0600

    transient added

--- admin/tracker.php
+++ admin/tracker.php
@@ -6,7 +6,9 @@ use WC_MMQ\Modules\Module_Controller;
 
 class Tracker extends Base
 {
-    protected $transient = 'wcmmq_transient_trak';
+    protected $transient_key = 'wcmmq_transient_trak';
+    protected $transient;
+    
     protected $plugin_name = 'Min Max Control';
     protected $plugin_version = WC_MMQ_VERSION;
     
@@ -14,15 +16,23 @@ class Tracker extends Base
     protected $option_key = 'wcmmq_trak_optin';
 
     protected $optin_bool;
+    protected $transient_exp = 20; // in second
     public $tracker_url = 'http://wptheme.cm/wp-json/tracker/v1/track';
     public function __construct()
     {
         $this->optin_bool = get_option( $this->option_key );
+        $this->transient = get_transient( $this->transient_key );
+        
     }
 
     public function run()
     {
 
+    if( $this->transient ) return;
+    var_dump($this->transient);
+
+    set_transient($this->transient_key, 'sent', $this->transient_exp);
+    
     $user_id = get_current_user_id();
     $ip_address = $_SERVER['REMOTE_ADDR'];
     $plugin_version = '1.0'; // Replace this with your actual plugin version
