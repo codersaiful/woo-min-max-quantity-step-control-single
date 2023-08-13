a329bcd18bede57f34007f715e40fb5446ad5fe5
Saiful Islam <codersaiful@gmail.com>
2023-08-13 17:41:37 +0600

    browse plugin page fix

--- admin/page-loader.php
+++ admin/page-loader.php
@@ -70,8 +70,8 @@ class Page_Loader extends Base
     {
         add_filter( 'plugins_api_result', [$this, 'plugins_api_result'], 1, 3 );
         $this->topbar_sub_title = __( 'Browse our Plugins','wcmmq' );
-        include $this->page_folder_dir . 'main-page/premium-link-header.php';
         include $this->topbar_file;
+        include $this->page_folder_dir . 'main-page/premium-link-header.php';
         include $this->page_folder_dir . 'browse-plugins.php';
     }
     
