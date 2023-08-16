71f8889b0be2f1c4d2baf7334418e4e631db0537
Saiful Islam <codersaiful@gmail.com>
2023-08-16 09:43:05 +0600

    WC tested up to: 	 8.0.2

--- wcmmq.php
+++ wcmmq.php
@@ -11,7 +11,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         6.3
  * WC requires at least: 3.0.0
- * WC tested up to: 	 8.0.1
+ * WC tested up to: 	 8.0.2
  * 
  * Text Domain: wcmmq
  * Domain Path: /languages/
