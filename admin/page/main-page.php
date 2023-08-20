2772699f108426b8bd5aa2f3f4e695232943df50
Saiful Islam <codersaiful@gmail.com>
2023-08-20 17:37:30 +0600

    recommendation added

--- admin/page/main-page.php
+++ admin/page/main-page.php
@@ -217,6 +217,45 @@ if( ! $is_pro ){
 
                     
         </form>
-        
+        <div class="wcmmq-section-panel supported-terms wcmmq-recomendation-area" id="wcmmq-recomendation-area">
+            <table class="wcmmq-table universal-setting">
+                <thead>
+                    <tr>
+                        <th class="wcmmq-inside">
+                            <div class="wcmmq-table-header-inside">
+                                <h3><?php echo esc_html__('Recommendation Area', 'wpt'); ?> <small class="wcmmq-small-title">To increase Sale</small></h3>
+                            </div>
+
+                        </th>
+                        <th>
+                            <div class="wcmmq-table-header-right-side"></div>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td>
+                        <div class="wcmmq-form-control">
+                            <div class="form-label col-lg-12">
+                            <?php do_action( 'wcmmq_plugin_recommend_here' ); ?>
+                            </div>
+                            <div class="form-label col-lg-12">
+                                <?php wcmmq_submit_issue_link(); ?>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="wqpmb-form-info">
+                            
+                            <?php wcmmq_social_links(); ?>
+                            <p>Highly Recommeded these plugin. Which will help you to increase your WooCommerce sale.</p>
+                        </div> 
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div> <!--/.wcmmq-recomendation-area -->
     </div>
 </div> 
