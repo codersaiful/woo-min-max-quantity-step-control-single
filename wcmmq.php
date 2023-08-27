2969b2af04a3a3712e6b8be59d3d1f323fd530be
Saiful Islam <codersaiful@gmail.com>
2023-08-27 14:54:11 +0600

    dd() to functions.php

--- wcmmq.php
+++ wcmmq.php
@@ -21,22 +21,6 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * Only for developer
- * @author Fazle Bari <fazlebarisn@gmail.com>
- */
-if( ! function_exists('dd') ){
-	function dd( ...$vals){
-		if( ! empty($vals) && is_array($vals) ){
-			foreach($vals as $val ){
-				echo "<pre>";
-				var_dump($val);
-				echo "</pre>";
-			}
-		}
-	}
-}
-
-/**
  * Defining constant
  */
 
