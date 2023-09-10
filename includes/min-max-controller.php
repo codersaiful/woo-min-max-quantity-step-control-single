3ebd525ea55d57990ed6c3b91af0a2a7655fa36e
Saiful Islam <codersaiful@gmail.com>
2023-09-10 18:08:40 +0600

    if( ! is_object( $product ) ) return $qty;

--- includes/min-max-controller.php
+++ includes/min-max-controller.php
@@ -582,6 +582,7 @@ class Min_Max_Controller extends Base
      */
     public function quantity_input_step($qty, $product)
     {
+        if( ! is_object( $product ) ) return $qty;
         if( ! method_exists($product, 'is_sold_individually') ) return $qty;
         if( $product->is_sold_individually() ) return $qty;
         $this->product = $product;
