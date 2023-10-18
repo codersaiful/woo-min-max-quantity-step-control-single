31206bcd08691067aaed911fce374bcab8704d17
Saiful Islam <codersaiful@gmail.com>
2023-10-18 12:28:08 +0600

    code updated

--- framework/plugin-required.php
+++ framework/plugin-required.php
@@ -51,9 +51,9 @@ if( ! class_exists( 'Plugin_Required' ) ){
          */
         public static function display_notice()
         {
-
+            
                 //after 10 days, offer will closed | Today: 24 Sept, 2023
-                if(time() > (1697365177 + 1728000)) return;
+                if(time() > (1697610263 + 1728000)) return;
                 if( defined( 'WC_MMQ_PRO_VERSION' ) ) return;
                 
                 $temp_numb = rand(5,9);
@@ -76,10 +76,10 @@ if( ! class_exists( 'Plugin_Required' ) ){
                  * Offer Hanndle
                  */
                 $coupon_Code = 'CYBERSECURITY50';
-                $target = 'https://codeastrology.com/coupons/?discount=' . $coupon_Code . '&campaign=' . $coupon_Code . '&ref=1&utm_source=Default_Offer_LINK';
-                $my_message = '<b><i>COUPON CODE: ' . $coupon_Code . ' - up to 50% OFF</i></b> A coupon code for you for <b>Min Max Control</b> Plugin';
+                $target = 'https://codeastrology.com/min-max-quantity/pricing/?discount=' . $coupon_Code . '&campaign=' . $coupon_Code . '&ref=1&utm_source=Default_Offer_LINK';
+                $my_message = '<b><i>COUPON CODE: ' . $coupon_Code . ' - up to 65% OFF</i></b> A coupon code for you for <b>Min Max Control</b> Plugin';
                 $offerNc = new Notice('wcmmq_'.$coupon_Code.'_offer');
-                $offerNc->set_title( 'Cyber Security Month - 50% Discount' )
+                $offerNc->set_title( 'upto 65% Discount - Cyber Security Month' )
                 ->set_diff_limit(3)
                 ->set_type('offer')
                 ->set_img( WC_MMQ_BASE_URL. 'assets/images/min-max-logo.png')
@@ -90,24 +90,25 @@ if( ! class_exists( 'Plugin_Required' ) ){
                     'type' => 'success',
                     'link' => $target,
                 ]);
+                // $offerNc->add_button([
+                //     'text' => 'Save Extra 35% on Bundle',
+                //     'type' => 'offer',
+                //     'link' => 'https://codeastrology.com/downloads/bundle-woo-product-table-min-max-step-control/?discount=' . $coupon_Code,
+                // ]);
                 $offerNc->add_button([
-                    'text' => 'Save Extra 35% on Bundle',
-                    'type' => 'offer',
-                    'link' => 'https://codeastrology.com/downloads/bundle-woo-product-table-min-max-step-control/?discount=' . $coupon_Code,
-                ]);
-                $offerNc->add_button([
-                    'text' => 'Unlimited Access(Lifetime)',
+                    'text' => 'Unlimited Access(Lifetime) with Discount',
                     'type' => 'default',
                     'link' => 'https://codeastrology.com/checkout?edd_action=add_to_cart&download_id=6557&edd_options%5Bprice_id%5D=6&discount=' . $coupon_Code,
                 ]);
                 
-                $offerNc->add_button([
-                    'text' => 'All Products',
-                    'type' => 'error',
-                    'link' => 'https://codeastrology.com/downloads//?discount=' . $coupon_Code,
-                ]);
-                if( method_exists($offerNc, 'set_location') ){
-                    // $offerNc->set_location('wpt_offer_here'); //wpt_premium_image_bottom
+                // $offerNc->add_button([
+                //     'text' => 'All Products',
+                //     'type' => 'error',
+                //     'link' => 'https://codeastrology.com/downloads//?discount=' . $coupon_Code,
+                // ]);
+                if( method_exists($offerNc, 'set_location') && $temp_numb == 8){
+                    $offerNc->set_location('wcmmq_form_panel_bottom'); //wpt_premium_image_bottom
+                    $offerNc->show();
                 }
                 if($temp_numb == 5) $offerNc->show();
                 
