074ef288c6f39d938fe80a3540069b839994d202
Saiful Islam <codersaiful@gmail.com>
2024-01-02 18:48:25 +0600

    class added

--- admin/adm-inc/plugin-installer.php
+++ admin/adm-inc/plugin-installer.php
@@ -3,18 +3,52 @@ namespace WC_MMQ\Admin\Adm_Inc;
 
 class Plugin_Installer
 {
+    public $plugin_zip_url;
+
+
+    /**
+     * Initializing static property for Class
+     * We will use this Trait acutally
+     *
+     * @var null|object
+     */
+    public static $init;
+
+    /**
+     * Initializing method init()
+     * If we want to make any Class as Standalone 
+     * then we have to use this trait
+     * 
+     * @since 1.0.0.11
+     * 
+     * @author Saiful Islam <codersaiful@gmail.com>
+     * @return null|object
+     */
+    public static function init()
+    {
+        if( self::$init && self::$init instanceof self ) return self::$init;
+
+        self::$init = new self();
+
+        return self::$init;
+    }
+
     public function run()
     {
         add_action('admin_init', [$this,'handle_install']);
+    }
+    public function set_plugin_zip_url( $url = '' )
+    {
+        $this->plugin_zip_url = $url;
     }
     public function handle_install()
     {
         if (isset($_POST['install_plugin']) && check_admin_referer('pssg_install_plugin_nonce', 'pssg_install_plugin_nonce')) {
             // URL to the zip file of the custom plugin
-            $plugin_zip_url = 'https://example.com/path/to/your-plugin.zip';
+            $this->plugin_zip_url = 'https://example.com/path/to/your-plugin.zip';
     
             // Install and activate the plugin
-            $result = $this->install_zip( $plugin_zip_url );
+            $result = $this->install_zip( $this->plugin_zip_url );
             if (is_wp_error($result)) {
                 // Handle error, if any
                 echo 'Error installing the plugin: ' . esc_html($result->get_error_message());
